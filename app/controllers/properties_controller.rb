9d9fa67350e8d189bb49857468c654e1fcd9d1b3
gabdemiranda <gabriel.demcastro@gmail.com>
2021-09-24 00:27:03 -0300

    update from last class

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,4 +3,13 @@ class PropertiesController < ApplicationController
     id = params[:id]
     @property = Property.find(id)
   end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+    p = Property.create(params.require(:property).permit(:title, :description, :rooms, :bathrooms, :pets, :parking_slot, :daily_rate))
+    redirect_to p
+  end
 end
